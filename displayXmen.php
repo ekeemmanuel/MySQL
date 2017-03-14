1ca625286540f6eef71eb00696ca85f587d31c76
1600558 <a@a.com>
2017-03-14 14:59:02 +0000

    Tuesday

---
+++ displayXmen.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1600558
+ * Date: 14/03/2017
+ * Time: 14:48
+ */
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-
-/**
- * Created by PhpStorm.
- * User: Eke
- * Date: 11/03/2017
- * Time: 20:48
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marvel Movies</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<header>
+    <h1>Index Page</h1>
+</header>
+<main>
+    <section>
+        <h3>Page Menu</h3>
+            <p><a href="displayAll.php">Display All Movies</a></p>
+            <p><a href="displayMarvel.php">Display Movies created by Marvel Studios</a></p>
+            <p><a href="displayCreatedafter.php">Display All Movies created after 2010</a></p>
+            <p><a href="displayXmen.php">Display all X-Men Films</a></p>
 <?php
     echo "Let the MySQL begin!"
 ?>
+
