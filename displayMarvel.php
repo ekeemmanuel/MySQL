1f5aa9a4fcdf59fc0705a6bf50ed1046f48ffdd4
1600558 <a@a.com>
2017-03-14 16:34:35 +0000

    Tuesday6

--- displayMarvel.php
+++ displayMarvel.php
@@ -2,7 +2,7 @@
 //link to your database
 include 'dbConnect.php';
 //create a SQL query as a string
-$sql_query = "SELECT * FROM marvelmovies WHERE productionStudio=20th Century Fox; ";
+$sql_query = "SELECT * FROM marvelmovies WHERE productionStudio='20th Century Fox'; ";
 //execute the SQL query
 $result = $link->query($sql_query);
 //iterate over $result object $row at a time
